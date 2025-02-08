609b15a76cbddef66f5bebab859894319e236fc5
cmacdonaldmuckle <macdonaldmucklecolin@gmail.com>
2025-02-08 14:49:22 -0500

    unfinish: working on Limelight in drive

--- src/main/java/frc/robot/subsystems/DriveSubsystem/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem/DriveSubsystem.java
@@ -125,44 +125,44 @@ public class DriveSubsystem extends CSubsystem {
     return DriveConstants.kDriveKinematics.toChassisSpeeds(states);
   }
 
-  // simple proportional turning control with Limelight.
-  // "proportional control" is a control algorithm in which the output is proportional to the error.
-  // in this case, we are going to return an angular velocity that is proportional to the 
-  // "tx" value from the Limelight.
-  double limelight_aim_proportional() {    
-    // kP (constant of proportionality)
-    // this is a hand-tuned number that determines the aggressiveness of our proportional control loop
-    // if it is too high, the robot will oscillate.
-    // if it is too low, the robot will never reach its target
-    // if the robot never turns in the correct direction, kP should be inverted.
-    double kP = .035;
+  // // simple proportional turning control with Limelight.
+  // // "proportional control" is a control algorithm in which the output is proportional to the error.
+  // // in this case, we are going to return an angular velocity that is proportional to the 
+  // // "tx" value from the Limelight.
+  // double limelight_aim_proportional() {    
+  //   // kP (constant of proportionality)
+  //   // this is a hand-tuned number that determines the aggressiveness of our proportional control loop
+  //   // if it is too high, the robot will oscillate.
+  //   // if it is too low, the robot will never reach its target
+  //   // if the robot never turns in the correct direction, kP should be inverted.
+  //   double kP = .035;
 
-    // tx ranges from (-hfov/2) to (hfov/2) in degrees. If your target is on the rightmost edge of 
-    // your limelight 3 feed, tx should return roughly 31 degrees.
-    double targetingAngularVelocity = LimelightHelpers.getTX("limelight") * kP;
+  //   // tx ranges from (-hfov/2) to (hfov/2) in degrees. If your target is on the rightmost edge of 
+  //   // your limelight 3 feed, tx should return roughly 31 degrees.
+  //   double targetingAngularVelocity = LimelightHelpers.getTX("limelight") * kP;
 
-    // convert to radians per second for our drive method
-    targetingAngularVelocity *= Constants.AutoConstants.kMaxAngularSpeedRadiansPerSecond;
+  //   // convert to radians per second for our drive method
+  //   targetingAngularVelocity *= Constants.AutoConstants.kMaxAngularSpeedRadiansPerSecond;
 
-    //invert since tx is positive when the target is to the right of the crosshair
-    targetingAngularVelocity *= -1.0;
+  //   //invert since tx is positive when the target is to the right of the crosshair
+  //   targetingAngularVelocity *= -1.0;
 
-    return targetingAngularVelocity;
-  }
+  //   return targetingAngularVelocity;
+  // }
    
-  // simple proportional ranging control with Limelight's "ty" value
-  // this works best if your Limelight's mount height and target mount height are different.
-  // if your limelight and target are mounted at the same or similar heights, use "ta" (area) for target ranging rather than "ty"
-  double limelight_range_proportional()
-  {    
-    double kP = .1;
-    double targetingForwardSpeed = LimelightHelpers.getTY("limelight") * kP;
-    targetingForwardSpeed *= Constants.AutoConstants.kMaxSpeedMetersPerSecond;
-    targetingForwardSpeed *= -1.0;
-    return targetingForwardSpeed;
-  }
+  // // simple proportional ranging control with Limelight's "ty" value
+  // // this works best if your Limelight's mount height and target mount height are different.
+  // // if your limelight and target are mounted at the same or similar heights, use "ta" (area) for target ranging rather than "ty"
+  // double limelight_range_proportional()
+  // {    
+  //   double kP = .1;
+  //   double targetingForwardSpeed = LimelightHelpers.getTY("limelight") * kP;
+  //   targetingForwardSpeed *= Constants.AutoConstants.kMaxSpeedMetersPerSecond;
+  //   targetingForwardSpeed *= -1.0;
+  //   return targetingForwardSpeed;
+  // }
 
-  public void buttonBindings( PS4Controller m_driverControllerm, PS4Controller m_driverController ) {
+  public void buttonBindings( PS4Controller m_driverController, PS4Controller m_codriverController ) {
     this.setDefaultCommand(
         // The left stick controls translation of the robot.
         // Turning is controlled by the X axis of the right stick.
@@ -174,18 +174,12 @@ public class DriveSubsystem extends CSubsystem {
               double rot = MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband);
               boolean fieldRelative = true;
 
-              // Switches to non field-relative driving if the driver presses the R3 button, and switches to using the limelight
-              if ( m_driverController.getR3ButtonPressed() ) {
+              // Switches to non field-relative driving if the driver presses the L1 button, and switches to using the limelight
+              if (LimelightHelpers.getTV("limelight") == true && m_driverController.getL1ButtonPressed() ) {
 
-                // SmartDashboard.putBoolean("LimelightDetecting", LimelightHelpers.get)
-                final var rot_limelight = limelight_aim_proportional();
-                rot = rot_limelight;
-
-                final var forward_limelight = limelight_range_proportional();
-                y = forward_limelight;
-
-                //while using Limelight, turn off field-relative driving.
-                fieldRelative = false;
+                LimelightHelpers.LimelightTarget_Fiducial apriltag = new LimelightHelpers.LimelightTarget_Fiducial();
+                
+                
               }
 
               this.drive(
