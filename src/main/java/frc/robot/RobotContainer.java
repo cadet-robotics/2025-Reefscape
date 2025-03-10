471825085e38d48785fc8d06b4a69301a53c4f7c
Jeff <25jpink@ga.hiltoncsd.net>
2025-03-10 18:14:41 -0400

    Auto done for now

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,22 +4,39 @@
 
 package frc.robot;
 
+// import java.io.BufferedWriter;
+import java.util.List;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
+
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 //import edu.wpi.first.wpilibj.PS4Controller.Button;
 
 import frc.robot.Constants.OIConstants;
-import edu.wpi.first.wpilibj.PS4Controller;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import com.pathplanner.lib.auto.AutoBuilder;
-
+import frc.robot.subsystems.AlgaeSubsystem;
 import frc.robot.subsystems.BucketSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.HorizontalExtenderSubsystem;
-import frc.robot.subsystems.AlgaeSubsystem;
 import frc.robot.subsystems.DriveSubsystem.DriveSubsystem;
 
 /*
@@ -40,7 +57,9 @@ public class RobotContainer {
   // The drivers' controllers
   private final PS4Controller m_driverController = new PS4Controller(OIConstants.kDriverControllerPort);
   private final PS4Controller m_coDriverController = new PS4Controller(OIConstants.kCoDriverControllerPort);
+
   private final SendableChooser<Command> autoChooser;
+
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -51,25 +70,28 @@ public class RobotContainer {
     passSlowModeBooleanSuppliers();
     extenderBucketBlocking();
 
-    // Configure the button bindings
-    String [] autos = new String[] {"test 1", "test 2"};
+    // Configure default commands
+    NamedCommands.registerCommand("Bucket Dump", m_bucket.BucketDump());
+    NamedCommands.registerCommand("Bucket Reset", m_bucket.BucketStart());
+    NamedCommands.registerCommand("Raise Elevator", m_elevatorSubsystem.ElevatorLevelUp());
+    NamedCommands.registerCommand("lower Elevator", m_elevatorSubsystem.ElevatorLevelDown());
+
+     String [] autos = new String[] {"Do Nothing","CenterLeftMid","CenterRightMid","LeftMid","RightMid","ScoreAuto"};
   
-    autoChooser = AutoBuilder.buildAutoChooser("test 1");
+    autoChooser = AutoBuilder.buildAutoChooser("Do Nothing");
 
     SmartDashboard.putStringArray("Auto List", autos );
     SmartDashboard.putData("Auto Selector", autoChooser);
-
-
-    // Configure default commands
   }
 
   /**
    * A simple method to pass the isBucketBlocking between the bucket and extender subsystems
    */
   private void extenderBucketBlocking() { 
-    //m_horizontalExtender.setIsBucketBlocking( m_bucket.isBucketBlocking );
+    m_horizontalExtender.setIsBucketBlocking( m_bucket.isBucketBlocking );
   }
   private void passSlowModeBooleanSuppliers() {
+
     DriveSubsystem.setSlowFunctions( 
       m_elevatorSubsystem.elevatorSlowCheck, 
       m_horizontalExtender.extenderSlowCheck 
@@ -86,11 +108,6 @@ public class RobotContainer {
    * {@link JoystickButton}.
    */
   private void configureButtonBindings() {
-    
-
-  
-    new JoystickButton( m_coDriverController, Constants.CoDriverControls.horizontalExtendButton )
-      .whileTrue( new SequentialCommandGroup( m_horizontalExtender.Extend(), m_intake.IntakeOut() ));
     
     // Intake buttons
     m_intake.buttonBindings(m_driverController, m_coDriverController);
@@ -109,8 +126,6 @@ public class RobotContainer {
 
     // Elevator Buttons
     m_elevatorSubsystem.buttonBindings(m_driverController, m_coDriverController);
-  }
-  public Command getAutonomousCommand() {
-    return autoChooser.getSelected();
+
   }
 }
