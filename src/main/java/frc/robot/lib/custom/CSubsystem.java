0021c3b3b725644f7b3f05c540ab4b27240911d0
J-Kistner <jk.gm.act@gmail.com>
2025-03-15 21:05:10 -0400

    all subsystems now have an onDisableInit() function for reseting/safefty

--- src/main/java/frc/robot/lib/custom/CSubsystem.java
+++ src/main/java/frc/robot/lib/custom/CSubsystem.java
@@ -1,20 +1,28 @@
+// The following code is from the following link but may be modified in some places.
 // https://github.com/Greater-Rochester-Robotics/GRRBase/blob/main/src/main/java/org/team340/lib/util/command/GRRSubsystem.java
+
 package frc.robot.lib.custom;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class CSubsystem implements Subsystem {
     
-    // Register the subsystem on creation
+    /** Register the subsystem on creation */
     public CSubsystem() {
         register();
     }
 
-    // Creating a new command
+    /**
+     * Function that is called when the robot is disabled. 
+     * This should be used to ensure that enabling the robot does not make it unsafe.
+     */
+    public void onDisableInit() {}
+
+    /** Creates a nameless command */
     public CCommand cCommand() {
         return new CCommand( this );
     }
 
-    // Creating a new command with a name
+    /** Creating a new command with a name */
     public CCommand cCommand_( String name ) {
         return new CCommand( name, this );
     }
