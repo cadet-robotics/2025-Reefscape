c3240868a4e88d72bb398b1340e7e286df688466
J-Kistner <jk.gm.act@gmail.com>
2025-03-15 21:38:23 -0400

    Removed the extender slow check

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,9 +4,6 @@
 
 package frc.robot;
 
-// import java.io.BufferedWriter;
-import java.util.List;
-
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 
@@ -16,9 +13,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-//import edu.wpi.first.wpilibj.PS4Controller.Button;
-import frc.robot.Configs.MAXSwerveModule;
 import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.AlgaeSubsystem;
 import frc.robot.subsystems.BucketSubsystem;
@@ -37,10 +31,10 @@ public class RobotContainer {
 
   // The robot's subsystems
   private final AlgaeSubsystem m_intake = new AlgaeSubsystem();
-  private final DriveSubsystem m_robotDrive = new DriveSubsystem();
-  private final HorizontalExtenderSubsystem m_horizontalExtender = new HorizontalExtenderSubsystem();
-  public final ElevatorSubsystem m_elevatorSubsystem = new ElevatorSubsystem();
   public final BucketSubsystem m_bucket = new BucketSubsystem();
+  private final DriveSubsystem m_robotDrive = new DriveSubsystem();
+  public final ElevatorSubsystem m_elevatorSubsystem = new ElevatorSubsystem();
+  private final HorizontalExtenderSubsystem m_horizontalExtender = new HorizontalExtenderSubsystem();
 
   // The drivers' controllers
   private final PS4Controller m_driverController = new PS4Controller(OIConstants.kDriverControllerPort);
@@ -56,7 +50,6 @@ public class RobotContainer {
     // Configure the button bindings
     configureButtonBindings();
     passSlowModeBooleanSuppliers();
-    extenderBucketBlocking();
 
     // Configure default commands
     NamedCommands.registerCommand("Bucket Dump", m_bucket.BucketDump());
@@ -65,7 +58,7 @@ public class RobotContainer {
     NamedCommands.registerCommand("lower Elevator", m_elevatorSubsystem.ElevatorLevelDown());
     NamedCommands.registerCommand("manual run",m_robotDrive.autoDrive());
 
-     String [] autos = new String[] {"Do Nothing","CenterLeftMid","CenterRightMid","LeftMid","RightMid","ScoreAuto","manual run"};
+    String [] autos = new String[] {"Do Nothing","CenterLeftMid","CenterRightMid","LeftMid","RightMid","ScoreAuto","manual run"};
   
     autoChooser = AutoBuilder.buildAutoChooser("Do Nothing");
 
@@ -73,17 +66,10 @@ public class RobotContainer {
     SmartDashboard.putData("Auto Selector", autoChooser);
   }
 
-  /**
-   * A simple method to pass the isBucketBlocking between the bucket and extender subsystems
-   */
-  private void extenderBucketBlocking() { 
-    m_horizontalExtender.setIsBucketBlocking( m_bucket.isBucketBlocking );
-  }
   private void passSlowModeBooleanSuppliers() {
 
     DriveSubsystem.setSlowFunctions( 
-      m_elevatorSubsystem.elevatorSlowCheck, 
-      m_horizontalExtender.extenderSlowCheck 
+      m_elevatorSubsystem.elevatorSlowCheck 
     );
   }
 
